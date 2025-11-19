a64e7bb7bbff66961de28718bbe065224768597a
Nelson Ramirez <xhyskoax@gmail.com>
2025-11-19 18:06:49 -0300

    m3 - lección 1 - Día 1

---
+++ tipos_datos.js
@@ -0,0 +1,27 @@
+//Numbers
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+let entero = 15;
+let decimal = 15.4;
+
+console.log("Es entero:", Number.isInteger(decimal));
+
+//String
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+let nombre = "Pedro";
+let texto = "Este texto habla sobre libros"; // habla -> trata
+
+texto = texto.replace("habla", "trata");
+
+console.log(texto);
+
+
+
+//Boolean
+let resultado = true;
+resultado = false;
+
+//Null
+let nulo = null;
+
+// Undefined
+let indefinido = undefined;
---
+++ variables.js
@@ -0,0 +1,6 @@
+let nombre = "Pedro";
+let apellido = "Soto";
+
+let nombreCompleto = `${nombre} ${apellido}`;
+
+const IVA = 19;
